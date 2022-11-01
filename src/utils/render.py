a52c53558033076660dd5838b63e18937b048605
MURAKI Taichi <muraki.taichi@gmail.com>
2022-11-01 14:20:19 +0900

    Add render util

---
+++ src/utils/render.py
@@ -0,0 +1,92 @@
+# from model import HandSilhouetteNet3
+
+import argparse
+import os
+import random
+import sys
+from pathlib import Path
+
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import torchvision
+from pytorch3d.io import load_obj, load_objs_as_meshes
+from pytorch3d.renderer import (
+    BlendParams,
+    DirectionalLights,
+    FoVPerspectiveCameras,
+    HardPhongShader,
+    Materials,
+    MeshRasterizer,
+    MeshRenderer,
+    PerspectiveCameras,
+    PointLights,
+    RasterizationSettings,
+    SoftPhongShader,
+    SoftSilhouetteShader,
+    TexturesUV,
+    TexturesVertex,
+    look_at_rotation,
+    look_at_view_transform,
+)
+from pytorch3d.structures import Meshes
+
+# from pytorch3d.vis.plotly_vis import AxisArgs, plot_batch_individually, plot_scene
+from pytorch3d.vis.texture_vis import texturesuv_image_matplotlib
+from torch.utils.data import DataLoader
+from torchvision import models, transforms
+from tqdm import tqdm
+
+import mano
+from dataset import FreiHandDataset_Estimated as FreiHandDataset
+
+
+def make_silhouette_phong_renderer(device, image_size=224):
+    # Initialize a perspective camera.
+    cameras = FoVPerspectiveCameras(device=device)
+
+    # To blend the 100 faces we set a few parameters which control the opacity and the sharpness of
+    # edges. Refer to blending.py for more details.
+    blend_params = BlendParams(sigma=1e-4, gamma=1e-4)
+
+    # Define the settings for rasterization and shading. Here we set the output image to be of size
+    # 256x256. To form the blended image we use 100 faces for each pixel. We also set bin_size and max_faces_per_bin to None which ensure that
+    # the faster coarse-to-fine rasterization method is used. Refer to rasterize_meshes.py for
+    # explanations of these parameters. Refer to docs/notes/renderer.md for an explanation of
+    # the difference between naive and coarse-to-fine rasterization.
+    raster_settings = RasterizationSettings(
+        image_size=image_size,
+        blur_radius=np.log(1.0 / 1e-4 - 1.0) * blend_params.sigma,
+        faces_per_pixel=100,
+    )
+
+    # Create a silhouette mesh renderer by composing a rasterizer and a shader.
+    silhouette_renderer = MeshRenderer(
+        rasterizer=MeshRasterizer(cameras=cameras, raster_settings=raster_settings),
+        shader=SoftSilhouetteShader(blend_params=blend_params),
+    )
+
+    # We will also create a Phong renderer. This is simpler and only needs to render one face per pixel.
+    raster_settings = RasterizationSettings(
+        image_size=image_size,
+        blur_radius=0.0,
+        faces_per_pixel=1,
+    )
+    # We can add a point light in front of the object.
+    lights = PointLights(device=device, location=((2.0, 2.0, -2.0),))
+    phong_renderer = MeshRenderer(
+        rasterizer=MeshRasterizer(cameras=cameras, raster_settings=raster_settings),
+        shader=HardPhongShader(device=device, cameras=cameras, lights=lights),
+    )
+    return silhouette_renderer, phong_renderer
+
+
+if __name__ == "__main__":
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    a = make_silhouette_phong_renderer(device)
+    print(a)
